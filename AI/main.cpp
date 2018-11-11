ca036933fe9ea4beff87f5112deef80d11496e0e
Shane Butler <shanecbutler@hotmail.com>
2018-11-11 14:31:37 +0000

    Initial Commit

---
+++ AI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AI
+//
+//  Created by Shane Butler on 11/11/2018.
+//  Copyright © 2018 Shane Butler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
